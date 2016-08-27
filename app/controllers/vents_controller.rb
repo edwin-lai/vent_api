082792b45962cf6533efd26cadfba055ac5b01b4
edwin-lai <edwinlai@ucla.edu>
2016-08-26 21:16:58 -0700

    complete api

--- app/controllers/vents_controller.rb
+++ app/controllers/vents_controller.rb
@@ -1,11 +1,20 @@
 class VentsController < ApplicationController
   def index
-    render json: Vent.order(created_at: :desc)
+    @vents = Vent.where.not(content: nil).order(created_at: :desc)
+    @vents = @vents.map do |vent|
+      {
+        content: vent['content'],
+        postTime: vent['created_at']
+      }
+    end
+    render json: @vents
   end
 
   def create
-    return unless params['text']
-    Vent.create(content: params['text'])
+    return if params['text'].empty?
+    @vent = Vent.new
+    @vent['content'] = params['text']
+    @vent.save
     index
   end
 end
--- app/models/vent.rb
+++ app/models/vent.rb
@@ -1,5 +1,3 @@
 class Vent < ApplicationRecord
   attr_accessor :content
-
-  validates :content, presence: true
 end
