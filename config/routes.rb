ab6668b79433c950ddb3ec84761cb3744f0b4718
edwin-lai <edwinlai@ucla.edu>
2016-09-23 00:02:21 -0700

    add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vents, only: [:index, :create]
+  resources :vents, only: [:index, :create, :destroy]
 end
